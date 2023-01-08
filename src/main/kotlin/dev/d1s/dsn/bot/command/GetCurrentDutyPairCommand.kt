6765dacfa7f31015a872f9ac691e1187e325ca22
d1snin <me@d1s.dev>
2023-01-09 01:30:11 +0300

    allowed the owner to execute read-only commands in a private chat

--- src/main/kotlin/dev/d1s/dsn/bot/command/GetCurrentDutyPairCommand.kt
+++ src/main/kotlin/dev/d1s/dsn/bot/command/GetCurrentDutyPairCommand.kt
@@ -20,7 +20,7 @@ import dev.d1s.dsn.service.DutyPairService
 import dev.d1s.dsn.service.GroupChatService
 import dev.d1s.dsn.util.Emoji
 import dev.d1s.dsn.util.formatCurrentDutyPair
-import dev.d1s.dsn.util.requireInitializedGroupChat
+import dev.d1s.dsn.util.requireInitializedGroupChatOrOwner
 import dev.d1s.dsn.util.withTitle
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
@@ -39,7 +39,7 @@ class GetCurrentDutyPairCommand : Command, KoinComponent {
     private val dutyPairService by inject<DutyPairService>()
 
     override suspend fun BehaviourContext.onCommand(message: TextMessage) {
-        requireInitializedGroupChat(groupChatService, message) {
+        requireInitializedGroupChatOrOwner(groupChatService, message) {
             val currentDutyPair = withTitle(Emoji.BUSTS_IN_SILHOUETTE, "Текущая дежурная пара:") {
                 formatCurrentDutyPair(dutyPairService)
             }
--- src/main/kotlin/dev/d1s/dsn/util/BehaviourContextExt.kt
+++ src/main/kotlin/dev/d1s/dsn/util/BehaviourContextExt.kt
@@ -16,14 +16,12 @@
 
 package dev.d1s.dsn.util
 
-import dev.d1s.dsn.entity.GroupChatInfo
 import dev.d1s.dsn.service.GroupChatService
 import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
 import dev.inmo.tgbotapi.extensions.utils.fromUserOrNull
 import dev.inmo.tgbotapi.types.BotCommand
-import dev.inmo.tgbotapi.types.UserId
 import dev.inmo.tgbotapi.types.chat.GroupChat
 import dev.inmo.tgbotapi.types.message.content.TextMessage
 import dev.inmo.tgbotapi.utils.PreviewFeature
@@ -42,21 +40,27 @@ suspend inline fun <BC : BehaviourContext> BC.requireGroupChat(message: TextMess
     }
 }
 
-suspend fun <BC : BehaviourContext> BC.requireInitializedGroupChat(
+suspend fun <BC : BehaviourContext> BC.requireInitializedGroupChatOrOwner(
     groupChatService: GroupChatService,
     message: TextMessage,
-    block: suspend BC.(GroupChatInfo) -> Unit
+    block: suspend BC.() -> Unit
 ) {
-    val thisChatId = message.chat.id.chatId
-
     val initializedGroupChat = groupChatService.getGroupChatInfo() ?: run {
         commandNotAvailable(message)
 
         return
     }
 
+    if (message.isFromOwner(groupChatService)) {
+        block()
+
+        return
+    }
+
+    val thisChatId = message.chat.id.chatId
+
     if (initializedGroupChat.groupChatId.chatId == thisChatId) {
-        block(initializedGroupChat)
+        block()
     } else {
         val wrongChatContent = makeTitle(Emoji.CROSS_MARK, "Этот чат не подходит для исполнения запрашиваемой команды.")
 
@@ -64,27 +68,35 @@ suspend fun <BC : BehaviourContext> BC.requireInitializedGroupChat(
     }
 }
 
-@OptIn(PreviewFeature::class)
 suspend fun <BC : BehaviourContext> BC.requireOwner(
     groupChatService: GroupChatService,
     message: TextMessage,
-    block: suspend BC.(UserId) -> Unit
+    block: suspend BC.() -> Unit
 ) {
-    val thisUser = message.fromUserOrNull()?.user?.id?.chatId
-
-    val ownerId = groupChatService.getGroupChatInfo()?.ownerId ?: run {
+    groupChatService.getGroupChatInfo()?.ownerId ?: run {
         commandNotAvailable(message)
 
         return
     }
 
-    if (ownerId.chatId == thisUser) {
-        block(ownerId)
+    if (message.isFromOwner(groupChatService)) {
+        block()
     } else {
         val noPermissionContent = makeTitle(Emoji.CROSS_MARK, "Нет привилегий.")
 
         reply(message, noPermissionContent)
     }
+}
+
+@OptIn(PreviewFeature::class)
+private suspend fun TextMessage.isFromOwner(
+    groupChatService: GroupChatService
+): Boolean {
+    val thisUser = fromUserOrNull()?.user?.id?.chatId
+
+    val ownerId = groupChatService.getGroupChatInfo()?.ownerId
+
+    return ownerId?.chatId == thisUser
 }
 
 private suspend fun <BC : BehaviourContext> BC.commandNotAvailable(message: TextMessage) {
